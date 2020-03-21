fa19b7598326a55094b5f9bd8faf7e537b759183
Guillermo Parral <guillermo.parral@mercadolibre.com>
2020-03-21 15:02:33 -0300

    add linting

--- react-dashboard/webpack.config.js
+++ react-dashboard/webpack.config.js
@@ -23,9 +23,7 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
+        loader: "babel-loader",
       },
       {
         test: /\.html$/,
